24482f715b6bbbe2d300cd25e1a4b871959c9cd0
Mokshit Jain <mokshitjain18@gmail.com>
2023-08-11 00:27:25 +0530

    AllApis crud done, Remaning google auth

--- ASP.NET/DapperCRUD/DapperCRUD/Controllers/UserController.cs
+++ ASP.NET/DapperCRUD/DapperCRUD/Controllers/UserController.cs
@@ -20,18 +20,25 @@ public class UserController : ControllerBase
     [HttpGet]
     public async Task<ActionResult<List<User>>> GetAllUsers()
     {
-        using var connction = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
-        var users = await connction.QueryAsync<User>("select * from users");
-        return Ok(users);
+        try
+        {
+            await using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var users = await connection.QueryAsync<User>("select * from users");
+            return Ok(users);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, $"Error: {ex.Message}");
+        }
     }
 
-    [HttpGet("{UserId}")]
-    public async Task<ActionResult<User>> GetUser(int UserId)
+    [HttpGet("{userId:int}")]
+    public async Task<ActionResult<User>> GetUser(int userId)
     {
         try
         {
-            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
-            var user = await connection.QueryFirstAsync<User>("select * from users where Id = @UsrId", new { UsrId = UserId });
+            await using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var user = await connection.QueryFirstAsync<User>("select * from users where Id = @UsrId", new { UsrId = userId });
             return Ok(user);
         }
         catch (Exception ex)
@@ -39,4 +46,49 @@ public class UserController : ControllerBase
             return StatusCode(StatusCodes.Status500InternalServerError, $"Error: {ex.Message}");
         }
     }
+
+    [HttpPost]
+    public async Task<ActionResult<User>> CreateUser(User user)
+    {
+        try
+        {
+            await using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("insert into Users (Id, Name, Password, Email) Values (@Id, @Name, @Password, @Email)", user);
+            return Ok(user);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, $"Error: {ex.Message}");
+        }
+    }
+
+    [HttpPut]
+    public async Task<ActionResult<User>> UpdateUser(User user)
+    {
+        try
+        {
+            await using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("Update Users set Name = @Name, Password = @Password, Email = @Email where Id = @Id", user);
+            return Ok(user);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, $"Error: {ex.Message}");
+        }
+    }
+    [HttpDelete("{userId:int}")]
+    public async Task<ActionResult<User>> DeleteUser(int userId)
+    {
+        try
+        {
+            await using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var user = await connection.QueryFirstAsync<User>("delete from User where Id = @UsrId", new { UsrId = userId });
+            return Ok(user);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, $"Error: {ex.Message}");
+        }
+    }
+
 }
