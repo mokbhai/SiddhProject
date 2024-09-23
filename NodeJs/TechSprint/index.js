815a97e1dced330e685f12bd13aae888315890d5
mokbhai <mokshitjain18@gmail.com>
2024-09-23 10:17:51 +0530

    cors

--- NodeJs/TechSprint/index.js
+++ NodeJs/TechSprint/index.js
@@ -51,11 +51,32 @@ const app = express();
 //   })
 // );
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://techsprint.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        // Allow the origin or if it's not set (some requests don't have it)
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+
 app.use(xss());
 app.use(mongoSanitize());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
 app.use(morgan("dev"));
 
 /// Api routes
